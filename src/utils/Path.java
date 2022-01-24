2ad160f993ac8b0491ddb600a1d7d5c8c89f5f3d
Nikola Damjanovic <cipinikolica@gmail.com>
2022-01-24 02:02:51 +0100

    Add some common paths

--- src/utils/Path.java
+++ src/utils/Path.java
@@ -5,5 +5,9 @@ public class Path {
     public static class Web {
         public static final String LOGIN = "/login/";
         public static final String SIGN_UP = "/signup/";
+        public static final String FEED = "/feed/";
+        public static final String SEARCH = "/search/";
+        public static final String PROFILE = "/profile/";
+        public static final String MESSAGES = "/messages/";
     }
 }
