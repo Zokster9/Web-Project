6555eb236a2c0ba649b6e2bae41d5c2a317727e6
Nikola Damjanovic <cipinikolica@gmail.com>
2022-02-02 03:48:57 +0100

    Add condition for type of user

--- static/app/feed.js
+++ static/app/feed.js
@@ -3,7 +3,7 @@ Vue.component("feed-page", {
     <div>
         <main-navbar></main-navbar>
         <div class="d-flex justify-content-center flex-column align-items-center" style="margin-top:80px;">
-            <router-link tag="button" class="btn btn-primary w-50 rounded-pill" to="/create-status/"><i class="far fa-plus-square"></i> Create a new status</router-link>
+            <router-link tag="button" class="btn btn-primary w-50 rounded-pill" to="/create-status/" style="max-width: 600px;"><i class="far fa-plus-square"></i> Create a new status</router-link>
             <status-ui v-for="status in statuses" :status="status"></status-ui>
         </div>
         <br>
@@ -16,14 +16,23 @@ Vue.component("feed-page", {
         }
     },
     mounted() {
-      axios.get("/feed/", {
-          headers: {
-              Authorization: 'Bearer ' + window.sessionStorage.getItem('jwt'),
-          }
-      })
-          .then((response) => {
-              this.statuses = response.data;
-              console.log(this.statuses);
-          })
+        if (window.sessionStorage.getItem("user") !== null){
+            if (JSON.parse(window.sessionStorage.getItem("user")).role === 'Administrator'){
+                router.push("/search")
+                return;
+            }
+        } else {
+            router.push("/search")
+            return;
+        }
+        axios.get("/feed/", {
+            headers: {
+                Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem("user")).JWTToken,
+            }
+        })
+            .then((response) => {
+                  this.statuses = response.data;
+                  console.log(this.statuses);
+            })
     },
 });
